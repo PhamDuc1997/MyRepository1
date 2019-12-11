93c2560c7711238d9be8b257860a393e6d6a451b
PhamDuc1997 <PhamDuc1997@users.noreply.github.com>
2019-12-11 14:41:16 +0700

    Update BT3 Assignment4.cpp

--- BT3 Assignment4.cpp
+++ BT3 Assignment4.cpp
@@ -3,6 +3,9 @@ int main(){
 	int a,n=0;
 	printf("vui long nhap so a\n");
 	scanf("%d",&a);
+	if(a<2){
+		printf("day khong phai so nguyen to\n");
+	}else{
 	for(int x=1;x<=a;x++){
 		if(a%x==0){
 			n++;
@@ -10,5 +13,6 @@ int main(){
 	}if(n==2){printf("%d la so nguyen to",a);
 	}else{printf("%d khong phai la so nguyen to",a);
 	}	
+	}
 	return 0;
 }
